5ab1d1b030a1718aa22de93193967f79bee8b7f7
xels79 <xel_s@mail.ru>
2024-06-03 19:06:56 +0300

    mime type

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hostname = '0.0.0.0';
 const port = 3030;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'application/http');
     res.end('<html><body><p><b>Hello World</b></p></body></html>');
 });
 server.listen(port, hostname, () => {
