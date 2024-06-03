e6c721a4084759d7de350e4cb0ee164b6437156e
xels79 <xel_s@mail.ru>
2024-06-03 18:56:40 +0300

    chenge listen addres and port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '0.0.0.0';
+const port = 3030;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
