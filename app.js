867c2d5c2220f040f96f9aeee1f6d600a4e376f2
xels79 <xel_s@mail.ru>
2024-06-03 19:00:28 +0300

    Add main tags

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3030;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end('<html><body><p><b>Hello World</b></p></body></html>');
 });
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
